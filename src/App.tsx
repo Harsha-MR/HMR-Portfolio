aa08f35d914ed6314d8249dbae5bea7a86dce77e
Harsha-MR <169922996+Harsha-MR@users.noreply.github.com>
2026-02-11 17:03:21 +0530

    Resume pdf is updated

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ export default function App() {
                 <Skills />
                 <Resume />
                 <Certificates />
-                <Blog />
+                {/* <Blog /> */}
                 <Contact />
               </>
             )}
